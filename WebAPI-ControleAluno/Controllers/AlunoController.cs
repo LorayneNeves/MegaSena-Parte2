83e6965c53a8e1f67df5181881556d488396e9a6
044860 <044860@UNI-LAB5-123>
2023-08-09 21:02:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPI-ControleAluno/Controllers/AlunoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI_ControleAluno.Models.Request;
+
+namespace WebAPI_ControleAluno.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        [HttpGet("{codigo}")]
+        public IActionResult Get(int codigo)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(AlunoViewModel alunoViewModel)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{codigo}")]
+        public IActionResult Put(AlunoViewModel alunoViewModel)
+        {
+            return Ok();
+        }
+
+
+        [HttpDelete("{codigo}")]
+        public IActionResult Delete(int codigo)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI-ControleAluno/Models/Request/AlunoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_ControleAluno.Models.Request
+{
+    public class AlunoViewModel
+    {
+        public string Nome { get; set; }
+        public int RA { get; set; }
+        public string email { get; set; }
+        public string CPF { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
