cf10adc04798ea08c29856c087a999652a66c890
044860 <044860@UNI-LAB5-125>
2023-08-22 21:05:23 -0300

    update validation

--- WebAPI-ControleAluno/Models/Request/AlunoViewModel.cs
+++ WebAPI-ControleAluno/Models/Request/AlunoViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace WebAPI_ControleAluno.Models.Request
+﻿using WebAPI_ControleAluno.Models.CustomValidation;
+
+namespace WebAPI_ControleAluno.Models.Request
 {
     public class AlunoViewModel
     {
@@ -6,6 +8,8 @@
         public string Nome { get; set; }
         public int RA { get; set; }
         public string email { get; set; }
+
+        [CpfValidation(ErrorMessage = "CPF inválido.")]
         public string CPF { get; set; }
         public bool Ativo { get; set; }
     }
